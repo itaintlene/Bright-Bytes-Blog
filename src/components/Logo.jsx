bc127151af050952a8b940aa6f4839957d5d7616
itaintlene <xharleeneydx@gmail.com>
2024-10-29 22:12:54 +0530

    logo changes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width = '100px'}) {
   return (
     <div>
-      <img src='https://github.com/itaintlene/Bright-Bytes-Blog/blob/f0e2db2e97d03e3b163f3c49070f1d72ed072992/Bright%20Bytes%20Logo.png' />
+      <img src='../Bright Bytes Logo.png' alt='Logo'/>
     </div>
   )
 }
