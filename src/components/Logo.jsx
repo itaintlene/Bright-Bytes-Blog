f0e2db2e97d03e3b163f3c49070f1d72ed072992
itaintlene <xharleeneydx@gmail.com>
2024-10-29 22:03:16 +0530

    add logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Logo({width = '100px'}) {
   return (
-    <div>Logo</div>
+    <div>
+      <img src='D:\UNIVERSITY NOTES AND STUFF\FRONT END DEV\BrightBytes\BrightBytes\Bright Bytes Logo.png' />
+    </div>
   )
 }
 
